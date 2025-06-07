e6505cd66a1aa3239413a9700509233b85cb3f70
Vitor José <vitorjoseengel@hotmail.com>
2025-06-07 12:25:07 -0300

    Web

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
+from matplotlib import pandas
+from django.http import HttpResponse
+from django.urls import path
+from .views import home
+
+
+def home(request):
+    return HttpResponse("<h1>Minha página inicial</h1><p>Projeto iniciado!</p>")
+
+
+
+
 print("Hello World")
